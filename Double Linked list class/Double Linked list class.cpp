04198aecf91631aac56c3361cc3333a9123e0e24
Maytham Hassn <124174844+maytham0@users.noreply.github.com>
2025-04-06 11:12:54 +0300

    Add files via upload

---
+++ Double Linked list class/Double Linked list class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "clsDBlLinkedList.h"
+#include "clsMyQueue.h"
+#include "clsMyStack.h"
+#include "clsDynamicArray.h"
+#include "clsMyQueueArr.h"
+#include "clsMyStackArr.h"
+using namespace std;
+
+int main()
+{
+//you can find my DS Lib is dev by @MaythaMH
+
+	system("pause >0");
+	return 0;
+}
